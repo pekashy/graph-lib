2a09507a612ee3ae8aae72ec354004134e94afcc
Egor Zyazev <zyazev.ea@phystech.edu>
2020-12-20 21:16:05 +0300

    Fix Edge interface

--- graph-lib-test.cpp
+++ graph-lib-test.cpp
@@ -28,15 +28,23 @@ TEST(GraphCreationTest, CreateAdjListGraph)
 TEST(EdgeCreationTest, CreateSimpleEdge)
 {
 	int content = 123;
-	auto e = ConcreteGraphEdge<int>::Create(&content);
+	auto e = ConcreteGraphEdge<int>::Create(&content, 0, 1);
 	ASSERT_TRUE(e.get() != nullptr);
 }
 
 TEST(EdgeCreationTest, CreateComplexEdge)
 {
 	auto* o = new FunnyGraphObject();
-	auto e = ConcreteGraphEdge<FunnyGraphObject>::Create(o);
+	auto e = ConcreteGraphEdge<FunnyGraphObject>::Create(o, 0, 0);
 	ASSERT_TRUE(e.get() != nullptr);
+}
+
+TEST(EdgeCreationTest, ParamsTest)
+{
+	auto* o = new FunnyGraphObject();
+	auto e = ConcreteGraphEdge<FunnyGraphObject>::Create(o, 1, 2);
+	ASSERT_TRUE(e->GetSource() == 1);
+	ASSERT_TRUE(e->GetDest() == 2);
 }
 
 int main(int argc, char **argv)
--- graph-node.h
+++ graph-node.h
@@ -2,11 +2,14 @@
 
 #include <memory>
 #include "common.h"
+#include <sstream>
 
 class GraphEdge
 {
 public:
 	virtual ~GraphEdge() = default;
+	virtual int GetSource() const = 0;
+	virtual int GetDest() const = 0;
 };
 
 template<typename T>
@@ -15,20 +18,34 @@ class ConcreteGraphEdge : public GraphEdge
 public:
 	virtual ~ConcreteGraphEdge() = default;
 
-	static std::shared_ptr<GraphEdge> Create(T* pContent) 
+	int GetSource() const override
+	{
+		return m_nSource;
+	}
+
+	int GetDest() const override
+	{
+		return m_nDest;
+	}
+
+	static std::shared_ptr<GraphEdge> Create(T* pContent, int nSource, int nDest) 
 	{
 		struct make_shared_enabler : public ConcreteGraphEdge<T> 
 		{
-			make_shared_enabler(T* pC) : ConcreteGraphEdge<T>(pC) {};
+			make_shared_enabler(T* pC, int nS, int nD) : ConcreteGraphEdge<T>(pC, nS, nD) {};
 		};
-		return std::make_shared<make_shared_enabler>(pContent); 
+		return std::make_shared<make_shared_enabler>(pContent, nSource, nDest);
 	};
 
 private:
-	ConcreteGraphEdge(T* pContent)
+	ConcreteGraphEdge(T* pContent, int nSource, int nDest)
 		: m_shContent(std::make_shared<T>(*pContent))
+		, m_nSource(nSource)
+		, m_nDest(nDest)
 	{}
 
+	int m_nSource;
+	int m_nDest;
 	std::shared_ptr<T> m_shContent;
 };
 
